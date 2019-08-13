1c21396d2e3fa71d0e92d8dc44a0101fb6dfb2e9
Sumedha Kanthamneni <48239303+sumedha-k@users.noreply.github.com>
2019-08-13 14:41:22 -0400

    Update Activity3.ino

--- Module 2 Workshops/Activity3/Activity3.ino
+++ Module 2 Workshops/Activity3/Activity3.ino
@@ -1,6 +1,5 @@
 pinBTN = 4;
 pinLED = 10;
-bool LEDon = false; // boolean to keep track of LED state
 
 void setup() {
   pinMode(pinBTN, INPUT);
@@ -9,16 +8,14 @@ void setup() {
 }
 
 void loop() {
-  // if the button is pressed but the LED isn't on yet
-  if (digitalRead(pinBTN)== HIGH && !LEDon)
+  // if the button is pressed 
+  if (digitalRead(pinBTN)== HIGH)
   {
     digitalWrite(pinLED, HIGH); // turn LED on
-    LEDon = true; // update boolean
   }
-  // if the button isn't pressed but the LED has't been turned off yet
-  if (digitalRead(pinBTN) == LOW && LEDon)
+  // if the button isn't pressed 
+  else
   {
     digitalWrite(pinLED, LOW); // turn LED off
-    LEDon = false; // update boolean
   }
 }
